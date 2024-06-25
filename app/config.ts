4e3ed35f2ba055b2028552e053b97be6e0f89f68
Marcus <pths411027@gmail.com>
2024-06-25 22:11:34 +0800

    feat: init commet

---
+++ app/config.ts
@@ -0,0 +1,33 @@
+import { title } from "process";
+
+export const frameworks = [
+  {
+    img: "React.jpeg",
+    title: "React",
+    des: "React is a JavaScript library for building user interfaces. Its core principles are declarative code and efficiency.",
+    url: "https://zh-hant.legacy.reactjs.org/",
+  },
+  {
+    img: "Vue.jpeg",
+    title: "Vue",
+    des: "Vue is a progressive JavaScript framework for building user interfaces. It is designed from the ground up to be incrementally adoptable.",
+    url: "https://vuejs.org/",
+  },
+  {
+    img: "Angular.jpeg",
+    title: "Angular",
+    des: "Angular is a platform and framework for building single-page client applications using HTML and TypeScript.",
+    url: "https://angular.tw/",
+  },
+  {
+    img: "Svelte.jpeg",
+    title: "Svelte",
+    des: "Svelte is a radical new approach to building user interfaces. It shifts the work of building applications from the client-side to the compile step.",
+    url: "https://svelte.dev/",
+  },
+];
+
+export const defaultText = {
+  title: "Framework Card",
+  detail: "See more",
+};
